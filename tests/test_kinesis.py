c62a5b67825e37028c13f1969baf64764642aa01
Michael Benowitz <mwbenowitz@gmail.com>
2018-12-31 14:14:11 -0500

    Fix tests after removing methods

--- tests/test_kinesis.py
+++ tests/test_kinesis.py
@@ -24,20 +24,21 @@ class TestKinesis(unittest.TestCase):
 
         body = json.dumps({
             'status': 200,
-            'stage': 'test',
+            'type': 'work',
+            'method': 'update',
             'data': record
         })
 
         expected_params = {
             'Data': body,
-            'StreamName': 'tester',
+            'StreamName': 'testStream',
             'PartitionKey': '0'
         }
 
         stubber.add_response('put_record', expResp, expected_params)
         stubber.activate()
 
-        kinesis.putRecord(record)
+        kinesis.putRecord(record, 'testStream')
 
     @patch.dict('os.environ', {'OUTPUT_KINESIS': 'tester', 'OUTPUT_SHARD': '0', 'OUTPUT_STAGE': 'test'})
     def test_putRecord_err(self):
@@ -61,7 +62,7 @@ class TestKinesis(unittest.TestCase):
         stubber.add_client_error('put_record', expected_params=expected_params)
         stubber.activate()
         try:
-            kinesis.putRecord(record)
+            kinesis.putRecord(record, 'testStream')
         except KinesisError:
             pass
         self.assertRaises(KinesisError)
