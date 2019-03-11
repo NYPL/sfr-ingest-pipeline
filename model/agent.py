c0763a4c436ce82ccd60ecaaa785dee232e701f4
Mike Benowitz <mwbenowitz@gmail.com>
2019-03-11 16:17:25 -0400

    Fix agent aliases and utcnow()
    Includes two minor bug fixes. First, corrects an error assigning
    aliases to Agents where an improper implementation of list(filter())
    was replaced with a more straightforward list comprehension.
    
    Second, to resolve discrepencies between AWS environments and local
    development environments, set all timestamp lines to utilize
    utcnow() instead of now()

--- model/agent.py
+++ model/agent.py
@@ -104,9 +104,13 @@ class Agent(Core, Base):
         for field, value in agent.items():
             if(value is not None and value.strip() != ''):
                 setattr(existing, field, value)
-
         if aliases is not None:
-            for alias in list(filter(lambda x: Alias.insertOrSkip(session, x, Agent, existing.id), aliases)):
+            aliasRecs = [
+                Alias.insertOrSkip(session, a, Agent, existing.id)
+                for a in aliases
+            ]
+            for alias in list(filter(None, aliasRecs)):
+                print(alias)
                 existing.aliases.append(alias)
 
         if type(link) is dict:
@@ -232,4 +236,5 @@ class Alias(Core, Base):
                 .filter(model.id == recordID)\
                 .one()
         except NoResultFound:
-            return cls(alias=alias)
+            print('Should return Alias?')
+            return Alias(alias=alias)
