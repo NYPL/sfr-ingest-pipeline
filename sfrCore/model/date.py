93ecf95bc6c49d6653dc87121df489d6aa1301fc
Mike Benowitz <mwbenowitz@gmail.com>
2019-05-14 13:40:55 -0400

    SFR-426 Improve date parsing
    This adds additional date parsing logic, to both clean/extract
    logical dates from strings and to handle some fuzzy date conditions.
    
    Improvements:
    - Extracts dates from brackets in plain-text date strings
    - Removes extraneous characters from dates to improve parsing
    - Handles question mark and dash fuzzy date strings, e.g. `19-?`

--- sfrCore/model/date.py
+++ sfrCore/model/date.py
@@ -131,28 +131,53 @@ class DateField(Core, Base):
     
     def update(self, dateData):
         """Update fields on existing date"""
-        newRange = DateField.parseDate(dateData['date_range'])
-        if newRange != self.date_range:
-            self.date_range = newRange
-            self.display_date = dateData['display_date']
+        DateField.cleanDateData(dateData)
+        self.setDateRange(dateData['date_range'])
+        self.display_date = dateData['display_date']
 
     @classmethod
     def insert(cls, dateData):
         """Insert a new date row"""
         newDate = DateField()
+        DateField.cleanDateData(dateData)
         for field, value in dateData.items():
             if field != 'date_range': setattr(newDate, field, value)
             else: newDate.setDateRange(dateData['date_range'])
         return newDate
+
+    @classmethod
+    def cleanDateData(cls, dateData):
+        bracketMatch = re.search(r'\[([\d\-\?]+)\]', dateData['date_range'])
+        if bracketMatch: dateData['date_range'] = bracketMatch.group(1)
+        if '?' in dateData['date_range']:
+            DateField.parseUncertainty(dateData)
+        dateData['date_range'] = dateData['date_range'].strip(' ©.')
+        dateData['display_date'] = dateData['display_date'].strip(' .[]')
+    
+    @classmethod
+    def parseUncertainty(cls, dateData):
+        rawDate = re.search(r'\d+', dateData['date_range']).group(0)
+        if len(rawDate) == 2:
+            dateData['date_range'] = '{}00-{}99'.format(rawDate, rawDate)
+        elif len(rawDate) == 3:
+            dateData['date_range'] = '{}0-{}9'.format(rawDate, rawDate)
+        elif len(rawDate) == 4:
+            dateInt = int(rawDate)
+            dateData['date_range'] = '{}-{}'.format(
+                str(dateInt - 1),
+                str(dateInt + 1)
+            )
 
     def setDateRange(self, dateObj):
         logger.info('Parsing date string {} into date range'.format(dateObj))
         try:
             if type(dateObj) is list:
                 logger.debug('Received start/end dates, treat as bounds')
+                startYear = parse(dateObj[0]).year
+                endYear = parse(dateObj[1]).year
                 self.date_range = '[{}, {})'.format(
-                    parse(dateObj[0]).date(),
-                    parse(dateObj[1]).date()
+                    date(startYear, 1, 1),
+                    date(endYear, 12, 31)
                 )
             elif re.match(r'^[0-9]{4}$', dateObj):
                 logger.debug('Received year value, parsing into full year')
@@ -160,6 +185,16 @@ class DateField(Core, Base):
                 self.date_range =  '[{}, {})'.format(
                     date(year, 1, 1),
                     date(year, 12, 31)
+                )
+            elif re.match(r'^[0-9]{4}-[0-9]{4}$', dateObj):
+                dateYears = dateObj.split('-')
+                startYear = parse(dateYears[0]).year
+                endYear = parse(dateYears[1]).year
+                if endYear < startYear:
+                    raise ValueError
+                self.date_range = '[{}, {})'.format(
+                    date(startYear, 1, 1),
+                    date(endYear, 12, 31)
                 )
             elif re.match(r'^[0-9]{4}-[0-9]{2}$', dateObj):
                 logger.debug('Received year-month, parsing into month range')
---
+++ tests/test_dates.py
@@ -0,0 +1,138 @@
+import unittest
+from unittest.mock import patch, MagicMock
+from collections import namedtuple
+
+from sfrCore.model import DateField
+
+TestDate = namedtuple('TestDate', ['id', 'display_date', 'date_range', 'date_type'])
+
+
+class TestDates(unittest.TestCase):
+    def test_date_repr(self):
+        testDate = DateField()
+        testDate.display_date = '2019'
+        self.assertEqual(str(testDate), '<Date(date=2019)>')
+
+    @patch.object(DateField, 'lookupDate', return_value=None)
+    @patch.object(DateField, 'insert', return_value='newDate')
+    def test_updateInsert_insert(self, mock_insert, mock_lookup):
+        dateInst = {'date_type': 'test', 'display_date': '0000'}
+        testDate = DateField.updateOrInsert('session', dateInst, 'test', 1)
+        mock_lookup.assert_called_once()
+        mock_insert.assert_called_once_with(dateInst)
+        self.assertEqual(testDate, 'newDate')
+    
+    def test_updateInsert_update(self):
+        dateInst = {'date_type': 'test', 'display_date': '0000'}
+        with patch.object(DateField, 'lookupDate') as mock_lookup:
+            mock_date = MagicMock()
+            mock_lookup.return_value = mock_date
+            testDate = DateField.updateOrInsert('session', dateInst, 'test', 1)
+            mock_lookup.assert_called_once()
+            self.assertEqual(testDate, mock_date)
+    
+    def test_date_lookup(self):
+        mock_session = MagicMock()
+        mock_session.query.return_value\
+            .join.return_value\
+            .filter.return_value.filter.return_value\
+            .one_or_none.return_value = 'testDate'
+        dateInst = {'date_type': 'test'}
+        mock_table = MagicMock()
+        mock_table.__tablename__ = 'teesting'
+        testDate = DateField.lookupDate(mock_session, dateInst, mock_table, 1)
+        self.assertEqual(testDate, 'testDate')
+    
+    @patch.object(DateField, 'cleanDateData')
+    @patch.object(DateField, 'setDateRange')
+    def test_update_date(self, mock_range, mock_clean):
+        testDate = DateField()
+        dateData = {
+            'display_date': '1066',
+            'date_range': '[1066-01-01,1066-12-31)'
+        }
+        testDate.update(dateData)
+        self.assertEqual(testDate.display_date, '1066')
+        self.assertEqual(testDate.date_range, None)
+
+    @patch.object(DateField, 'setDateRange')
+    @patch.object(DateField, 'cleanDateData')
+    def test_insert_date(self, mock_clean, mock_parse):
+        newDate = {
+            'display_date': 'new',
+            'date_range': '[3,4)',
+            'date_type': 'tester'
+        }
+        res = DateField.insert(newDate)
+        self.assertEqual(res.display_date, 'new')
+        self.assertEqual(res.date_range, None)
+        self.assertEqual(res.date_type, 'tester')
+    
+    @patch.object(DateField, 'parseUncertainty')
+    def test_clean_date(self, mock_uncertain):
+        dateData = {
+            'date_range': 'date [1999?]',
+            'display_date': '[1999]'
+        }
+        DateField.cleanDateData(dateData)
+        self.assertEqual(dateData['date_range'], '1999?')
+        self.assertEqual(dateData['display_date'], '1999')
+
+    def test_parse_uncertain_4(self):
+        dateData = {
+            'date_range': '1999?',
+            'display_date': '1999'
+        }
+        DateField.parseUncertainty(dateData)
+        self.assertEqual(dateData['date_range'], '1998-2000')
+    
+    def test_parse_uncertain_3(self):
+        dateData = {
+            'date_range': '199?',
+            'display_date': 'the 90s'
+        }
+        DateField.parseUncertainty(dateData)
+        self.assertEqual(dateData['date_range'], '1990-1999')
+    
+    def test_parse_uncertain_2(self):
+        dateData = {
+            'date_range': '19-?',
+            'display_date': '1900s'
+        }
+        DateField.parseUncertainty(dateData)
+        self.assertEqual(dateData['date_range'], '1900-1999')
+
+    def test_parse_single_date(self):
+        testDate = DateField()
+        testDate.setDateRange('2018-01-10')
+        self.assertEqual(testDate.date_range, '[2018-01-10,)')
+
+    def test_parse_date_list(self):
+        testDate = DateField()
+        testDate.setDateRange(['2018', '2019'])
+        self.assertEqual(testDate.date_range, '[2018-01-01, 2019-12-31)')
+
+    def test_parse_year(self):
+        testDate = DateField()
+        testDate.setDateRange('2018')
+        self.assertEqual(testDate.date_range, '[2018-01-01, 2018-12-31)')
+    
+    def test_parse_years(self):
+        testDate = DateField()
+        testDate.setDateRange('2006-2010')
+        self.assertEqual(testDate.date_range, '[2006-01-01, 2010-12-31)')
+    
+    def test_parse_years_reversed(self):
+        testDate = DateField()
+        testDate.setDateRange('2010-2006')
+        self.assertEqual(testDate.date_range, '[2006-01-01, 2010-12-31)')
+
+    def test_parse_month(self):
+        testDate = DateField()
+        testDate.setDateRange('2018-02')
+        self.assertEqual(testDate.date_range, '[2018-02-01, 2018-02-28)')
+
+    def test_parse_bad_date(self):
+        testDate = DateField()
+        testDate.setDateRange('Modnay, Dec 01, 87')
+        self.assertEqual(testDate.date_range, None)
