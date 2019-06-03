a03ded0cf151db425bf2d387d1581f1b72174f51
Mike Benowitz <mwbenowitz@gmail.com>
2019-06-03 14:24:52 -0400

    SFR-461 Update tests with Nock
    Forgot to mock ElasticSearch responses for routing testing. This
    utilizes `nock` to ensure that the tests are mocked and adds a line
    to ensure the `env` variables are properly loaded for mocking.

--- test/esIntegrations.test.js
+++ test/esIntegrations.test.js
@@ -1,7 +1,9 @@
 /* eslint-disable no-undef */
+require('dotenv').config()
 const request = require('supertest')
 const chai = require('chai')
 const sinonChai = require('sinon-chai')
+const nock = require('nock')
 
 chai.should()
 chai.use(sinonChai)
@@ -9,26 +11,37 @@ const { expect } = chai
 
 describe('Testing ElasticSearch Integration', () => {
   let server
+  let req
   beforeEach(() => {
     // eslint-disable-next-line global-require
     server = require('../app.js')
+    req = request(server)
   })
   afterEach(() => {
     server.close()
   })
 
-  it('responds to v2 search', (done) => {
-    request(server)
-      .get('/v2/sfr/search?field=keyword&query=the')
-      .then((resp) => {
-        expect(resp.body.hits.total).to.be.greaterThan(0)
-        done()
+  it('response to v1 search', async () => {
+    const v1Mock = nock(process.env.ELASTICSEARCH_HOST)
+      .post('/sfr/_search')
+      .reply(200, {
+        took: 0,
+        timed_out: false,
+        hits: {
+          total: 1,
+          max_score: 1,
+          hits: [
+            {
+              _index: 'sfr_test',
+              _type: 'test',
+              _id: 11,
+              _score: 1,
+            },
+          ],
+        },
+        aggregations: {},
       })
-  })
-
-  it('response to v1 search', (done) => {
-    request(server)
-      .post('/api/v0.1/sfr/works')
+    req.post('/api/v0.1/sfr/works')
       .send({
         queries: [{
           field: 'keyword',
@@ -37,7 +50,46 @@ describe('Testing ElasticSearch Integration', () => {
       })
       .then((resp) => {
         expect(resp.body.hits.total).to.be.greaterThan(0)
-        done()
+        // eslint-disable-next-line no-underscore-dangle
+        expect(resp.body.hits.hits[0]._id).to.equal(11)
+        // eslint-disable-next-line no-unused-expressions
+        expect(v1Mock.isDone()).to.be.true
+      })
+  })
+
+  it('responds to v2 search', async () => {
+    const v2Mock = nock(process.env.ELASTICSEARCH_HOST)
+      .post('/_count')
+      .reply(200, { count: 1 })
+      .post('/sfr_test/_search')
+      .reply(200, {
+        took: 0,
+        timed_out: false,
+        hits: {
+          total: 1,
+          max_score: 1,
+          hits: [
+            {
+              _index: 'sfr_test',
+              _type: 'test',
+              _id: 31,
+              _score: 1,
+              _source: {
+                instances: [],
+              },
+            },
+          ],
+        },
+        aggregations: {},
+      })
+
+    req.get('/v2/sfr/search?field=keyword&query=the')
+      .then((resp) => {
+        expect(resp.body.hits.total).to.be.greaterThan(0)
+        // eslint-disable-next-line no-underscore-dangle
+        expect(resp.body.hits.hits[0]._id).to.equal(31)
+        // eslint-disable-next-line no-unused-expressions
+        expect(v2Mock.isDone()).to.be.true
       })
   })
 })
