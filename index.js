9b2ca4c653852be04c248d72595891cb4e338fbd
Mike Benowitz <mwbenowitz@gmail.com>
2019-10-12 20:28:19 -0400

    SFR-662 Port over existing functions from spike branch

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import GitFetch from './src/githubDataFetch'
+import { getRDF, getRepos, addCoverFile } from './src/githubDataFetch'
 import Kinesis from './src/kinesisOutput'
 import Helpers from './src/fetchHelpers'
 import logger from './src/helpers/logger'
@@ -7,46 +7,50 @@ exports.retrieveRepos = async () => {
   let success = false
   let tries = 0
 
+  /* eslint-disable no-await-in-loop */
   do {
     logger.notice('Attempting to retrieve records from GITenberg')
     if (success === false) await Helpers.sleep(10000, tries)
-    success = await GitFetch.getRepos()
-    tries++
+    success = await getRepos()
+    tries += 1
   } while (success === false && tries < process.env.REPO_RETRIES)
+  /* eslint-enable no-await-in-loop */
 
   return success
 }
 
 exports.getRepoData = async (repoInfo, lcRels) => {
-  let rdfValue = await GitFetch.getRDF(repoInfo, lcRels)
+  const rdfValue = await getRDF(repoInfo, lcRels)
+  await addCoverFile(repoInfo, rdfValue)
   return rdfValue
 }
 
 exports.handler = async (event, context, callback) => {
-  let success = await exports.retrieveRepos()
+  const success = await exports.retrieveRepos()
 
   if (success === false) {
     logger.error('Github API request returned too many 5XX errors')
     return callback(new Error('Github API request returned too many 5XX errors'))
   }
 
-  let lcRels = await Helpers.loadLCRels()
-  let repoInfo = success
+  const lcRels = await Helpers.loadLCRels()
+  const repoInfo = success
   if (repoInfo.length === 0) {
     logger.notice('No updates made in the fetch period to GITenberg')
-    let emptyResult = {
-      'source': 'gutenberg',
-      'status': 204,
-      'message': 'No records updated in fetch period'
+    const emptyResult = {
+      source: 'gutenberg',
+      status: 204,
+      message: 'No records updated in fetch period',
     }
     Kinesis.resultHandler(emptyResult)
     return callback(null, 'No updated records found')
   }
 
-  for (let i = 0; i < repoInfo.length; i++) {
-    let metadataRec = await exports.getRepoData(repoInfo[i], lcRels)
+  for (let i = 0; i < repoInfo.length; i += 1) {
+    // eslint-disable-next-line no-await-in-loop
+    const metadataRec = await exports.getRepoData(repoInfo[i], lcRels)
     logger.debug('Processed GITenberg record')
-    Kinesis.resultHandler(metadataRec)
+    // Kinesis.resultHandler(metadataRec)
   }
 
   logger.notice('Successfully updated records')
--- src/githubDataFetch.js
+++ src/githubDataFetch.js
@@ -1,24 +1,28 @@
+import { InMemoryCache } from 'apollo-cache-inmemory'
 import { ApolloClient } from 'apollo-client'
-import gql from 'graphql-tag'
-import fetch from 'node-fetch'
-import { createHttpLink } from 'apollo-link-http'
 import { setContext } from 'apollo-link-context'
 import { onError } from 'apollo-link-error'
-import { InMemoryCache } from 'apollo-cache-inmemory'
+import { createHttpLink } from 'apollo-link-http'
 import ApolloLinkTimeout from 'apollo-link-timeout'
+import Axios from 'axios'
+import fs from 'fs'
+import gql from 'graphql-tag'
+import fetch from 'node-fetch'
 import moment from 'moment'
+import yaml from 'js-yaml'
 
 import RDFParser from './parseRDF'
 import logger from './helpers/logger'
+import { LCSHYamlType, LCCYamlType } from './helpers/yaml'
 
 const httpLink = createHttpLink({
   uri: 'https://api.github.com/graphql',
-  fetch: fetch
+  fetch,
 })
 
 const errorLink = onError(({ graphQLErrors, networkError }) => {
   if (graphQLErrors) {
-    graphQLErrors.map(({ message, location, path }) => {
+    graphQLErrors.forEach(({ message, location, path }) => {
       logger.error(`Error in GraphQL Query: ${message}, Location: ${location}, Path: ${path}`)
     })
   } else if (networkError) {
@@ -32,12 +36,13 @@ const timeoutLink = new ApolloLinkTimeout(20000)
 
 const errorTimeoutHttpLink = timeoutLink.concat(errorHttpLink)
 
+// eslint-disable-next-line no-unused-vars
 const authLink = setContext((_, { headers }) => {
   const token = process.env.GITHUB_API_TOKEN
   return {
     headers: {
-      authorization: token ? `Bearer ${token}` : ''
-    }
+      authorization: token ? `Bearer ${token}` : '',
+    },
   }
 })
 
@@ -47,27 +52,27 @@ const authLink = setContext((_, { headers }) => {
 const apolloOpts = {
   watchQuery: {
     fetchPolicy: 'no-cache',
-    errorPolicy: 'ignore'
+    errorPolicy: 'ignore',
   },
   query: {
     fetchPolicy: 'no-cache',
-    errorPolicy: 'all'
-  }
+    errorPolicy: 'all',
+  },
 }
 
 const client = new ApolloClient({
   link: authLink.concat(errorTimeoutHttpLink),
   cache: new InMemoryCache(),
-  defaultOptions: apolloOpts
+  defaultOptions: apolloOpts,
 })
 
 const pgIDRegex = /([0-9]+)$/
 
-exports.getRepos = () => {
-  let first = 25
-  let fetchBoundary = moment().subtract(process.env.UPDATE_MAX_AGE_DAYS, 'days')
-  let repoIDs = []
-  return new Promise((resolve, reject) => {
+const getRepos = () => {
+  const first = 25
+  const fetchBoundary = moment().subtract(process.env.UPDATE_MAX_AGE_DAYS, 'days')
+  const repoIDs = []
+  return new Promise((resolve) => {
     client.query({
       query: gql`
               {
@@ -79,87 +84,148 @@ exports.getRepos = () => {
                 }
               }
             }
-          `
-    }).then(data => {
-
+          `,
+    }).then((data) => {
       // If data is null, the GraphQL request errored out and should return false
-      if (data['data'] == 'null') resolve(false)
+      if (data.data === 'null') resolve(false)
 
-      let repoList = data['data']['organization']['repositories']['nodes']
+      const repoList = data.data.organization.repositories.nodes
       repoList.forEach((repo) => {
-        let updatedAt = moment(repo['pushedAt'])
+        const updatedAt = moment(repo.pushedAt)
         if (updatedAt.isBefore(fetchBoundary)) return
-        let name = repo['name']
+        const { name, url } = repo
 
-        let idnoMatch = pgIDRegex.exec(name)
+        const idnoMatch = pgIDRegex.exec(name)
         if (!idnoMatch) return
 
-        let idno = idnoMatch[0]
-
-        let url = repo['url']
+        const idno = idnoMatch[0]
 
         repoIDs.push([name, idno, url])
       })
       resolve(repoIDs)
     })
-      .catch(err => {
-        resolve(false)
-      })
+      .catch(() => resolve(false))
   })
 }
 /* eslint-disable prefer-promise-reject-errors */
-exports.getRDF = (repo, lcRels) => {
-  return new Promise((resolve, reject) => {
-    let repoName = repo[0]
-    let gutID = repo[1]
-    let repoURI = repo[2]
-    let rdfPath = 'master:pg' + gutID + '.rdf'
-    client.query({
-      query: gql`
-              {
-                repository(owner:\"GITenberg\", name:\"${repoName}\"){
-                  object(expression:\"${rdfPath}\"){
-                    id
-                    ... on Blob {text}
-                  }
-              }
+const getRDF = (repo, lcRels) => new Promise((resolve) => {
+  const repoName = repo[0]
+  const gutID = repo[1]
+  const repoURI = repo[2]
+  const rdfPath = `master:pg${gutID}.rdf`
+  client.query({
+    query: gql`
+            {
+              repository(owner:\"GITenberg\", name:\"${repoName}\"){
+                object(expression:\"${rdfPath}\"){
+                  id
+                  ... on Blob {text}
+                }
             }
-          `
-    }).then(data => {
-      RDFParser.parseRDF(data, gutID, repoURI, lcRels, (err, rdfData) => {
-        if (err) {
-          resolve({
-            'recordID': gutID,
-            'source': 'gutenberg',
-            'type': 'work',
-            'method': 'insert',
-            'data': err,
-            'status': 500,
-            'message': 'Could not parse Gutenberg Metadata'
-          })
-        } else {
-          resolve({
-            'recordID': gutID,
-            'source': 'gutenberg',
-            'type': 'work',
-            'method': 'insert',
-            'data': rdfData,
-            'status': 200,
-            'message': 'Retrieved Gutenberg Metadata'
-          })
-        }
-      })
-    }).catch(err => {
-      resolve({
-        'recordID': gutID,
-        'source': 'gutenberg',
-        'type': 'work',
-        'method': 'insert',
-        'data': err,
-        'status': 500,
-        'message': 'Error in parsing Gutenberg Record'
-      })
+          }
+        `,
+  }).then((data) => {
+    RDFParser.parseRDF(data, gutID, repoURI, lcRels, (err, rdfData) => {
+      if (err) {
+        resolve({
+          recordID: gutID,
+          source: 'gutenberg',
+          type: 'work',
+          method: 'insert',
+          data: err,
+          status: 500,
+          message: 'Could not parse Gutenberg Metadata',
+        })
+      } else {
+        resolve({
+          recordID: gutID,
+          source: 'gutenberg',
+          type: 'work',
+          method: 'insert',
+          data: rdfData,
+          status: 200,
+          message: 'Retrieved Gutenberg Metadata',
+        })
+      }
+    })
+  }).catch((err) => {
+    resolve({
+      recordID: gutID,
+      source: 'gutenberg',
+      type: 'work',
+      method: 'insert',
+      data: err,
+      status: 500,
+      message: 'Error in parsing Gutenberg Record',
     })
   })
+})
+
+const fetchCoverFile = async (gutenPath, repoName, filePath) => {
+  const fileURL = gutenPath.replace('ebooks', 'files')
+  const coverURL = `${fileURL}/${filePath}`
+  const fileWriter = fs.createWriteStream(repoName)
+  const fileResp = await Axios({ url: coverURL, method: 'GET', responseType: 'stream' })
+  fileResp.data.pipe(fileWriter)
+  return new Promise((resolve, reject) => {
+    fileWriter.on('finish', resolve(repoName))
+    fileWriter.on('error', reject)
+  })
 }
+
+const getMetadataFile = (repo) => new Promise((resolve, reject) => {
+  client.query({
+    query: gql`
+            {
+              repository(owner:\"GITenberg\", name:\"${repo}\"){
+                object(expression:\"master:metadata.yaml\"){
+                  id
+                  ... on Blob {text}
+                }
+            }
+          }
+        `,
+  }).then((data) => {
+    try {
+      const gutenSchema = yaml.Schema.create([LCSHYamlType, LCCYamlType])
+      const yamlConfig = yaml.load(data.data.repository.object.text, { schema: gutenSchema })
+      resolve(yamlConfig)
+    } catch (err) {
+      reject(err)
+    }
+  }).catch((err) => {
+    reject(err)
+  })
+})
+
+const addCoverFile = async (repo, metadata) => {
+  const repoName = repo[0]
+  let repoMetadata
+  try {
+    repoMetadata = await getMetadataFile(repoName)
+  } catch (err) {
+    logger.error(err)
+  }
+  if (repoMetadata.covers) {
+    logger.debug(`Found covers in ${repoName}`)
+    // eslint-disable-next-line consistent-return
+    repoMetadata.covers.forEach(async (coverMeta) => {
+      logger.debug(`Cover Type: ${coverMeta.cover_type} | Cover Path: ${coverMeta.image_path}`)
+      if (coverMeta.cover_type !== 'generated') {
+        // eslint-disable-next-line no-underscore-dangle
+        const coverFile = await fetchCoverFile(repoMetadata.url, repoMetadata._repo, coverMeta.image_path)
+        // return coverFile
+        console.log(coverFile)
+      }
+    })
+  }
+  return null
+}
+
 /* eslint-enable prefer-promise-reject-errors */
+
+module.exports = {
+  getRepos,
+  getRDF,
+  addCoverFile,
+}
