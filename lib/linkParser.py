5e1e63238727debf7cb47a7ca6e1304aa22c5745
Mike Benowitz <mwbenowitz@gmail.com>
2019-12-10 16:50:14 -0500

    SFR-697 Parse FrontiersIn links
    This adds a new set of parsers that read the root links after following all redirects from DOAB. If these links match a specific pattern they are reformatted to a standard that we know can provide a higher degree of utility for ResearchNow users. For the FrontiersIn links this means that we can supply a local EPUB copy as well as a downloadable PDF copy for all of their open source works.
    
    This implements a plugin structure that makes it much easier to add parsers for additional sources in the future. There is also a "base" parser that handles any links not other wise parsed in a naive way.
    
    Finally this adds internal VIAF processing to help with the overall performance of the backend application

---
+++ lib/linkParser.py
@@ -0,0 +1,33 @@
+from lib.dataModel import Link
+
+from lib.parsers.defaultParser import DefaultParser
+from lib.parsers.frontierParser import FrontierParser
+
+class LinkParser:
+    PARSERS = [
+        FrontierParser,
+        DefaultParser
+    ]
+    def __init__(self, item, uri, media_type):
+        self.item = item
+        self.uri = uri
+        self.media_type = media_type
+    
+    def selectParser(self):
+        for model in self.PARSERS:
+            parser = model(self.uri, self.media_type)
+            if parser.validateURI() is True:
+                self.parser = parser
+                break
+
+    def createLinks(self):
+        for link in self.parser.createLinks():
+            print(link)
+            self.item.addClassItem('links', Link, **{
+                'url': link[0],
+                'media_type': link[2],
+                'flags': link[1] 
+            })
+
+
+
